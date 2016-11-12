f2093c1bd1618100055781ce209b25709b01be3f
TheBeff <thebeff@gmail.com>
2016-11-12 15:00:33 -0500

    pushing for mike

--- server/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = requrire('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'node_modules')));
-
-module.exports = app; 
