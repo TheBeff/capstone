26ca74bd8b75a2f934f12e86807ef7139f18de52
TheBeff <thebeff@gmail.com>
2016-11-12 16:25:00 -0500

    added angular codemirrors, sending test to console

--- public/main.js
+++ public/main.js
@@ -1,2 +1,22 @@
-const app = angular.module('app', ['ui.router']);
+const app = angular.module('app', ['ui.router','ui.codemirror']);
 
+app.controller('MainCtrl', function($scope){
+
+	$scope.submit = function(value){
+		console.log(value);
+	};
+
+	$scope.testEditorOptions = {
+		lineNumbers: true,
+		mode: "javascript"
+	};
+
+	$scope.codeEditorOptions = {
+		lineNumbers: true,
+		mode: "javascript",
+		readOnly: "nocursor"
+	};
+
+	$scope.code = "function foo(){\n  return 'bar';\n}"
+	$scope.testCode = "var assert = require('assert');\n\ndescribe('foo', function(){\n  it ('should return bar', function(){\n    assert.deepEqual(foo(), 'bar');\n  });\n});";
+});
