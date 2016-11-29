3c56fa17ec5ad51aa89a3033ba893cc57a7fed71
Michael Crowley <miketcrowley@gmail.com>
2016-11-29 01:26:40 -0500

    heroku push

--- testSpec.js
+++ testSpec.js
@@ -5,20 +5,9 @@ function foo(){
   return 'bar';
 }
 
-function bar(){
-  return 'foo';
-}
-
 describe('foo', function(){
   it ('should return bar', function(done){
     expect(foo()).to.equal('bar');
-    done();
-  });
-});
-
-describe('bar', function(){
-  it ('should return foo', function(done){
-    expect(bar()).to.equal('foo');
     done();
   });
 });
